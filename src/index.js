92bd4d79776a6bfc2a283309e08b9024521d9372
AjaxKronus <107383559+AjaxKronus@users.noreply.github.com>
2026-03-25 16:54:09 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css'; // Import the custom CSS
+import App from './components/App';
+import pets from './data/dogs.json'; // Import the JSON data
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App pets={pets} />);
